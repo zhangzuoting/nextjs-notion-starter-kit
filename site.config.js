44c35a6ab2a8f2815f2fa1a8107fe29bcf13af6f
zhangzuoting <43030743+zhangzuoting@users.noreply.github.com>
2021-06-07 20:26:07 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jackie的学习笔记',
+  domain: 'zhangzuoting.us',
+  author: 'Jackie Zhang',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Jackie',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  github: 'zhangzuoting',
+  //linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
